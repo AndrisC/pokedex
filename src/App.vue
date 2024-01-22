00672a3df379b9ddfd79d0c67e9ae6890406d754
andrisc <csipszerandras@gmail.com>
2024-01-22 12:02:13 +0100

    project created

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-container">
+    <h2>Welcome to the Pokedex</h2>
+    <poke-card />
+  </div>
+</template>
+
+<script>
+import PokeCard from './components/pokeCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PokeCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/pokeCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="poke-card">
+    card content
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  
+</style>
